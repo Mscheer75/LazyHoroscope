3e3e04745217a96416251d4ad2796fa65dbeb0ea
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-02-13 11:33:04 -0800

    added horoscope code

--- main.cpp
+++ main.cpp
@@ -11,16 +11,19 @@ int main()
 {
  string firstn;
  string lastn; //declare variables
-
+ int lucky;
 
 
 cout << "Please enter your first name: " << endl; //gets using inputs
 cin >> firstn;
 cout << "Please enter your last name: " << endl;
 cin >> lastn;
-cout << "Welcome, " << firstn[0] << "." << lastn[0] << "." << ", here is your fortune..." << endl;  
+cout << "Welcome, " << firstn[0] << "." << lastn[0] << "." << ", here is your fortune..." << endl;
 
-  //tell fortune
+
+cout << "The varible has " << firstn.length() << " letters. " << endl;  //tell fortune
+lucky = firstn.length();
+cout << "Your lucky number is " << lucky << endl;
 
   return 0;
 }
