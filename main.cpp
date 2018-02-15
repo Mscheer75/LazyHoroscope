e84a74917ebbc395a0c0e3b332ffb9679f9b33f7
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-02-15 11:38:12 -0800

    hopfully a finished program

--- main.cpp
+++ main.cpp
@@ -12,6 +12,12 @@ int main()
  string firstn;
  string lastn; //declare variables
  int lucky;
+ string first;
+ string last;
+ int lastleng;
+
+
+
 
 
 cout << "Please enter your first name: " << endl; //gets using inputs
@@ -21,9 +27,36 @@ cin >> lastn;
 cout << "Welcome, " << firstn[0] << "." << lastn[0] << "." << ", here is your fortune..." << endl;
 
 
-cout << "The varible has " << firstn.length() << " letters. " << endl;  //tell fortune
+first = firstn[0];//def of word lengths being stored into varibles
 lucky = firstn.length();
-cout << "Your lucky number is " << lucky << endl;
+lastleng = lastn.length();
+last = lastn[lastleng-1];
+
+
+cout << "your lucky number is " << lucky << endl;
+
+if (first == "a" || first == "A" || first == "e" || first == "E" || first == "i" || first == "I" || first == "o" || first == "O" || first == "u" || first == "U"  )  
+
+    {
+    cout << "you destined to be famous!" << endl;
+
+
+
+    } else 
+    {
+
+    cout << "you should keep a low profile." << endl;
+
+    }
+    
+    if (last == "a" || last == "A" || last == "e" || last == "E" || last == "i" || last == "I" || last == "o" || last == "O" || last == "u" || last == "U")
+    {
+    cout << "you have already met your true love." << endl;
+    }
+    cout << "have a good day!" << endl;
+
+
+
 
   return 0;
 }
